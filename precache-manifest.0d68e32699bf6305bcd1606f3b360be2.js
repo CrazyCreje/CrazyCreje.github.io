b20bdd23f0eca57c72fbce19630963bdc53abfdf
cvalenti312@gmail.com <cvalenti312@gmail.com>
2019-12-09 20:03:44 -0500

    Updates

---
+++ precache-manifest.0d68e32699bf6305bcd1606f3b360be2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a602266c549943876b941a40a4d62fd7",
+    "url": "/DigiSafe/index.html"
+  },
+  {
+    "revision": "09cbf0656854c5ae8294",
+    "url": "/DigiSafe/static/css/main.f6aa7029.chunk.css"
+  },
+  {
+    "revision": "080f79f0cd041f3f12bd",
+    "url": "/DigiSafe/static/js/2.7c771757.chunk.js"
+  },
+  {
+    "revision": "09cbf0656854c5ae8294",
+    "url": "/DigiSafe/static/js/main.b1a01f7c.chunk.js"
+  },
+  {
+    "revision": "6c77189fa14dcacee222",
+    "url": "/DigiSafe/static/js/runtime-main.a9f10050.js"
+  },
+  {
+    "revision": "46b4c7ced7eb377a6f3c600fa8651b88",
+    "url": "/DigiSafe/static/media/blue-and-purple-cosmic-sky-956999.46b4c7ce.jpg"
+  },
+  {
+    "revision": "06a1b1b3c4c090f6155936bdc5e5895e",
+    "url": "/DigiSafe/static/media/hero_laugh.06a1b1b3.png"
+  },
+  {
+    "revision": "f53ba2d204a87d12963c195a7e2b3e85",
+    "url": "/DigiSafe/static/media/laughingcave.f53ba2d2.jpg"
+  },
+  {
+    "revision": "dbfccf93750dcc2b8b840afa7262a986",
+    "url": "/DigiSafe/static/media/wonderingcave.dbfccf93.jpg"
+  }
+]);
